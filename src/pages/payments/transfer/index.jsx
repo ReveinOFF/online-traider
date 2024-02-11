661ce5108db4eef9a5dc669fde5b720680c9f06b
RonniePlay <ronnieplayyt@gmail.com>
2024-02-11 22:15:28 +0200

    Update functional

--- src/pages/payments/transfer/index.jsx
+++ src/pages/payments/transfer/index.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Selector from "../../../components/selector";
 import CustomInput from "../../../components/input";
 import { SmGreenButton } from "../../../components/buttons";
 import styles from "./transfer.module.scss";
-import sexData from "../../../utils/sex";
 import { useSearchParams } from "react-router-dom";
 import DataCreate from "../../../components/data-create";
 import axios from "axios";
@@ -12,15 +11,21 @@ import { useTranslation } from "react-i18next";
 
 const Transfer = () => {
   const [data, setData] = useState();
+  const [dataAcc, setDataAcc] = useState();
+  const [disabled, setDisabled] = useState(false);
   const [searchParams] = useSearchParams();
   const { t, i18n } = useTranslation();
+  const [selectedCurr, setSelectedCurr] = useState("USD");
+  const [selectedAcc, setSelectedAcc] = useState();
+  const [money, setMoney] = useState();
 
   useEffect(() => {
+    setDisabled(true);
     const { key, rand_param } = DataCreate();
 
     axios
       .get(
-        "https://cabinet.itcyclonelp.com/api/v_2/payments/GetPaymentSystemsUnitedByGroups",
+        "https://cabinet.itcyclonelp.com/api/v_2/payments/GetPaymentSystemsList",
         {
           params: {
             key,
@@ -34,36 +39,123 @@ const Transfer = () => {
       )
       .then((e) => {
         if (e.data.result === "success") {
-          setData(e.data.values);
+          setData(
+            e.data.values.find((item) => item.id === searchParams.get("id"))
+          );
+          setSelectedCurr(
+            e.data.values.find((item) => item.id === searchParams.get("id"))
+              .currency[0]
+          );
+        }
+      });
+
+    axios
+      .get("https://cabinet.itcyclonelp.com/api/v_2/trading/GetBalanceInfo", {
+        params: {
+          key,
+          rand_param,
+          auth_token: LocalStorage.get("auth_token"),
+          user_id: LocalStorage.get("user_id"),
+          languages: i18n.language,
+        },
+      })
+      .then((e) => {
+        if (e.data.result === "success") {
+          setDataAcc(e.data.values);
+          setSelectedAcc(
+            e.data.values?.find((item) => item.curr === selectedCurr)
+              ?.server_account
+          );
+          setDisabled(false);
         }
       });
   }, []);
 
+  const costChange = useCallback(
+    (v) => {
+      if (!v) return;
+
+      const numericValues = Object.values(v)
+        ?.filter((value) => /^\d+%$/.test(value))
+        .map((value) => parseInt(value, 10));
+
+      const min = numericValues.length > 0 ? Math.min(...numericValues) : 0;
+      const max = numericValues.length > 0 ? Math.max(...numericValues) : 0;
+
+      return min !== max ? `${min}-${max}%` : `${min}%`;
+    },
+    [data]
+  );
+
+  const handleClick = () => {};
+
   return (
     <>
-      <h1>Bank transfer</h1>
+      <h1>{t("transfer.h1")}</h1>
       <div className={`item-center ${styles.block_transfer}`}>
         <div className={styles.info}>
-          <div>Валюта: USD/EUR/RUB</div>
-          <div>Комиссия: 5%</div>
-          <div>Срок перевода: 1-5 work days</div>
+          <div>
+            {t("transfer.i1")} {data?.currency.join("/")}
+          </div>
+          <div>
+            {t("transfer.i2")} {costChange(data?.costs)}
+          </div>
+          <div>
+            {t("transfer.i3")} {data?.caption[i18n.language]}
+          </div>
         </div>
         <div className={styles.s_transfer}>
           <fieldset className="fs-t">
-            <div>Счет</div>
-            <Selector />
+            <div>{t("transfer.select1")}</div>
+            <Selector
+              selected={`${
+                dataAcc?.find((item) => item.server_account === selectedAcc)
+                  ?.server_account
+              } (${
+                dataAcc?.find((item) => item.server_account === selectedAcc)
+                  ?.curr
+              }) - ${parseFloat(
+                dataAcc?.find((item) => item.server_account === selectedAcc)
+                  ?.balance
+              )
+                .toLocaleString("en-US", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })
+                .replace(",", " ")}`}
+            >
+              {dataAcc?.map((item) => (
+                <div
+                  key={item.server_account}
+                >{`${item.server_account} (${item.curr}) - ${item.balance}`}</div>
+              ))}
+            </Selector>
           </fieldset>
           <fieldset className="fs-t">
-            <div>Валюта платежа</div>
-            <Selector />
+            <div>{t("transfer.select2")}</div>
+            <Selector selected={selectedCurr}>
+              {data?.currency.map((item, index) => (
+                <div key={index} onClick={() => selectedCurr(item)}>
+                  {item}
+                </div>
+              ))}
+            </Selector>
           </fieldset>
           <fieldset className={`flex-center ${styles.fs_lr}`}>
-            <div>Сумма платежа</div>
-            <CustomInput type="number" />
-            <div>USD</div>
+            <div>{t("transfer.money")}</div>
+            <CustomInput
+              type="number"
+              onChange={(e) => setMoney(e.target.value)}
+            />
+            <div>{selectedCurr}</div>
           </fieldset>
         </div>
-        <SmGreenButton>ДАЛЕЕ</SmGreenButton>
+        <SmGreenButton
+          onClick={handleClick}
+          disabled={disabled || !money || !selectedAcc || !selectedCurr}
+        >
+          {t("transfer.btn")}
+        </SmGreenButton>
       </div>
     </>
   );
--- src/pages/trade/my/index.jsx
+++ src/pages/trade/my/index.jsx
@@ -30,97 +30,76 @@ const MyAccount = () => {
       )
       .then((e) => {
         if (e.data.result === "success") {
-          setDataFirst(e.data.values);
-          console.log(e.data.values);
+          setDataFirst(Object.values(e.data.values));
         }
       });
 
-    await axios
-      .get(
-        "https://cabinet.itcyclonelp.com/api/v_2/trading/GetTradeAccountInfo",
-        {
-          params: {
-            key,
-            rand_param,
-            auth_token: LocalStorage.get("auth_token"),
-            user_id: LocalStorage.get("user_id"),
-            languages: i18n.language,
-            server_account: "",
-          },
-        }
-      )
-      .then((e) => {
-        if (e.data.result === "success") {
-          setDataFirst(e.data.values);
-          console.log(e.data.values);
-        }
-      });
+    dataFirst?.forEach(async (item) => {
+      await axios
+        .get(
+          "https://cabinet.itcyclonelp.com/api/v_2/trading/GetTradeAccountInfo",
+          {
+            params: {
+              key,
+              rand_param,
+              auth_token: LocalStorage.get("auth_token"),
+              user_id: LocalStorage.get("user_id"),
+              languages: i18n.language,
+              server_account: item.server_account,
+            },
+          }
+        )
+        .then((e) => {
+          if (e.data.result === "success") {
+            setDataSecond((prev) => [...prev, e.data.values]);
+          }
+        });
+    });
   };
 
   useEffect(() => {
-    const { key, rand_param } = DataCreate();
-
-    axios
-      .get(
-        "https://cabinet.itcyclonelp.com/api/v_2/trading/GetTradeAccountByUser",
-        {
-          params: {
-            key,
-            rand_param,
-            auth_token: LocalStorage.get("auth_token"),
-            user_id: LocalStorage.get("user_id"),
-            languages: i18n.language,
-            server_account: "10036",
-          },
-        }
-      )
-      .then((e) => {
-        if (e.data.result === "success") {
-          setDataFirst(e.data.values);
-          console.log(Object.values(e.data.values));
-        }
-      });
+    getData();
   }, []);
 
   return (
     <>
-      <h1>Мои счета</h1>
+      <h1>{t("my_trade.h1")}</h1>
       <div className="table justify-center">
         <table>
           <thead>
             <tr>
-              <th>Счет</th>
-              <th>Тип счета</th>
-              <th>Можно вывести</th>
-              <th>Средства</th>
-              <th>Залог</th>
-              <th>Бонусный баланс</th>
-              <th>Прибыль</th>
-              <th>Actions</th>
+              <th>{t("my_trade.th1")}</th>
+              <th>{t("my_trade.th2")}</th>
+              <th>{t("my_trade.th3")}</th>
+              <th>{t("my_trade.th4")}</th>
+              <th>{t("my_trade.th5")}</th>
+              <th>{t("my_trade.th6")}</th>
+              <th>{t("my_trade.th7")}</th>
+              <th>{t("my_trade.th8")}</th>
             </tr>
           </thead>
-          {/* <tbody>
-            {dataFirst?.map((item) => (
+          <tbody>
+            {dataSecond?.map((item) => (
               <tr key={item.id}>
                 <td>{item.server_account}</td>
-                <td>{item.account_currency}</td>
+                <td>{item.curr}</td>
+                <td>{item.freeBalance}</td>
                 <td>{item.balance}</td>
-                <td>179 642.02</td>
                 <td>{item.margin}</td>
                 <td>{item.bonus}</td>
                 <td>-13.15</td>
                 <td>
                   <SmGreenButton className={styles.td_btn}>
-                    <Link to="/trade/open">ПОПОЛНИТЬ СЧЕТ</Link>
+                    <Link to="/trade/open">{t("my_trade.btn")}</Link>
                   </SmGreenButton>
                 </td>
               </tr>
             ))}
-          </tbody> */}
-          {dataFirst.length === 0 && (
+          </tbody>
+          {dataSecond.length === 0 && (
             <tfoot>
               <tr>
-                <td colSpan={8}>Нет результатов</td>
+                <td colSpan={8}>{t("tfoot")}</td>
               </tr>
             </tfoot>
           )}
@@ -128,7 +107,7 @@ const MyAccount = () => {
       </div>
       <div className={styles.btn}>
         <BigButton className={styles.btn}>
-          <Link to="/trade/open">ПОПОЛНИТЬ СЧЕТ</Link>
+          <Link to="/trade/open">{t("my_trade.btn")}</Link>
         </BigButton>
       </div>
     </>
